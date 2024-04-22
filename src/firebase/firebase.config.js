3aba97cf1b2a7cf3c2c659727015558e8c644873
ayathree <nobanitaayathree333@gmail.com>
2024-04-22 22:05:30 +0600

    following

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDa-8FwJakZkRFAk4PdnCMt6UFiNV3-YNw",
-  authDomain: "coffee-store-3f25b.firebaseapp.com",
-  projectId: "coffee-store-3f25b",
-  storageBucket: "coffee-store-3f25b.appspot.com",
-  messagingSenderId: "429153674911",
-  appId: "1:429153674911:web:4641528b7ec4580a22e4aa"
+    apiKey:import.meta.env.VITE_APIKEY,
+    authDomain:import.meta.env.VITE_AUTHDOMAIN,
+    projectId:import.meta.env.VITE_PROJECTID,
+    storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+    appId:import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
